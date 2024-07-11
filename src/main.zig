af190190f96e8dda1f197fcfaaa54fa5e956a023
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-07-11 07:37:21 +0400

    Adjust names and definication to zig documentation

--- src/main.zig
+++ src/main.zig
@@ -14,8 +14,8 @@ pub const std_options = .{
 const hash_length = Sha256.digest_length;
 const nonce_length: usize = 256;
 
-const complexity_type = u8;
-const min_complexity: complexity_type = 1;
+const complexity = u8;
+const min_complexity: complexity = 1;
 
 // We use it to wait in threads loops until os signal will be received.
 // Note: in case of using two variables to wait for os signal: for example we are using separate bool variable and a mutex,
@@ -23,7 +23,7 @@ const min_complexity: complexity_type = 1;
 // otherwise deadlock, because mutex will be acquired in the same thread.
 var wait_signal = std.atomic.Value(bool).init(true);
 
-fn handle_signal(
+fn handleSignal(
     signal: i32,
     _: *const std.posix.siginfo_t,
     _: ?*anyopaque,
@@ -41,51 +41,51 @@ const Block = struct {
     hash: [hash_length]u8,
     prev_hash: [hash_length]u8,
     timestamp: i64,
-    complexity: complexity_type,
+    complexity: complexity,
     nonce: [nonce_length]u8,
 
-    fn fields_size() comptime_int {
+    fn size() comptime_int {
         return comptime @sizeOf(u128) +
             hash_length +
             hash_length +
             @sizeOf(i64) +
-            @sizeOf(complexity_type) +
+            @sizeOf(complexity) +
             nonce_length;
     }
 
-    fn to_bytes(self: Block) [fields_size()]u8 {
-        var buf = [_]u8{0} ** fields_size();
+    fn toBytes(self: Block) [size()]u8 {
+        var buf = [_]u8{0} ** size();
 
-        const self_index_fr = 0;
-        const self_index_to = self_index_fr + @sizeOf(u128);
-        std.mem.writeInt(u128, buf[self_index_fr..self_index_to], self.index, .little);
+        const index_from = 0;
+        const index_to = index_from + @sizeOf(u128);
+        std.mem.writeInt(u128, buf[index_from..index_to], self.index, .little);
 
-        const self_hash_fr = self_index_to;
-        const self_hash_to = self_hash_fr + hash_length;
-        @memcpy(buf[self_hash_fr..self_hash_to], &self.hash);
+        const hash_from = index_to;
+        const hash_to = hash_from + hash_length;
+        @memcpy(buf[hash_from..hash_to], &self.hash);
 
-        const self_prev_hash_fr = self_hash_to;
-        const self_prev_hash_to = self_prev_hash_fr + hash_length;
-        @memcpy(buf[self_prev_hash_fr..self_prev_hash_to], &self.prev_hash);
+        const prev_hash_from = hash_to;
+        const prev_hash_to = prev_hash_from + hash_length;
+        @memcpy(buf[prev_hash_from..prev_hash_to], &self.prev_hash);
 
-        const self_timestamp_fr = self_prev_hash_to;
-        const self_timestamp_to = self_timestamp_fr + @sizeOf(i64);
-        std.mem.writeInt(i64, buf[self_timestamp_fr..self_timestamp_to], self.timestamp, .little);
+        const timestamp_from = prev_hash_to;
+        const timestamp_to = timestamp_from + @sizeOf(i64);
+        std.mem.writeInt(i64, buf[timestamp_from..timestamp_to], self.timestamp, .little);
 
-        const self_complexity_fr = self_timestamp_to;
-        const self_complexity_to = self_complexity_fr + @sizeOf(complexity_type);
-        std.mem.writeInt(complexity_type, buf[self_complexity_fr..self_complexity_to], self.complexity, .little);
+        const complexity_from = timestamp_to;
+        const complexity_to = complexity_from + @sizeOf(complexity);
+        std.mem.writeInt(complexity, buf[complexity_from..complexity_to], self.complexity, .little);
 
-        const self_nonce_fr = self_complexity_to;
-        const self_nonce_to = self_nonce_fr + nonce_length;
-        @memcpy(buf[self_nonce_fr..self_nonce_to], &self.nonce);
+        const nonce_from = complexity_to;
+        const nonce_to = nonce_from + nonce_length;
+        @memcpy(buf[nonce_from..nonce_to], &self.nonce);
 
         return buf;
     }
 
-    fn to_hash(self: *Block) [Sha256.digest_length]u8 {
+    fn toHash(self: *Block) [Sha256.digest_length]u8 {
         var hasher = Sha256.init(.{});
-        const block_bytes = self.to_bytes();
+        const block_bytes = self.toBytes();
         hasher.update(&block_bytes);
         const hash = hasher.finalResult();
         self.hash = hash;
@@ -93,13 +93,15 @@ const Block = struct {
     }
 
     fn verify(self: *Block, prev_block: Block) error{HashesNotEqual}!void {
-        const hash = self.to_hash();
+        const hash = self.toHash();
         if (!std.mem.eql(u8, &self.hash, &hash)) return error.HashesNotEqual;
         if (!std.mem.eql(u8, &self.prev_hash, &prev_block.hash)) return error.HashesNotEqual;
         if (self.index - 1 != prev_block.index) return error.HashesNotEqual;
+        // todo: check complexity
+        // todo: check timestamp
     }
 
-    fn is_hash_empty(self: Block) bool {
+    fn isHashEmpty(self: Block) bool {
         return std.mem.eql(u8, &[_]u8{0} ** hash_length, &self.hash);
     }
 };
@@ -129,28 +131,27 @@ pub fn main() !void {
         .nonce = [_]u8{0} ** nonce_length,
     };
     // To set genesis.hash.
-    _ = genesis.to_hash();
+    _ = genesis.toHash();
     try blocks.append(genesis);
 
     var state = State{ .blocks = blocks };
 
-    const http_server_thread = try std.Thread.spawn(.{}, http_server, .{});
-    const tcp_server_thread = try std.Thread.spawn(.{}, tcp_server, .{});
-    const mining_loop_thread = try std.Thread.spawn(.{}, mining_loop, .{
+    const http_server_thread = try std.Thread.spawn(.{}, httpServer, .{});
+    const tcp_server_thread = try std.Thread.spawn(.{}, tcpServer, .{});
+    const mining_loop_thread = try std.Thread.spawn(.{}, miningLoop, .{
         @as(*std.rand.Xoshiro256, &rnd),
         @as(*State, &state),
     });
-    const broadcast_loop_thread = try std.Thread.spawn(.{}, broadcast_loop, .{});
+    const broadcast_loop_thread = try std.Thread.spawn(.{}, broadcastLoop, .{});
 
     var act = std.posix.Sigaction{
-        .handler = .{ .sigaction = handle_signal },
+        .handler = .{ .sigaction = handleSignal },
         .mask = std.posix.empty_sigset,
         .flags = (std.posix.SA.SIGINFO),
     };
     var oact: std.posix.Sigaction = undefined;
     try std.posix.sigaction(std.posix.SIG.INT, &act, &oact);
-
-    wait_signal_loop();
+    waitSignalLoop();
 
     // Waiting for other threads to be stopped.
     http_server_thread.join();
@@ -167,36 +168,36 @@ pub fn main() !void {
     log.info("finally successfully exiting...", .{});
 }
 
-fn wait_signal_loop() void {
+fn waitSignalLoop() void {
     log.info("starting to wait for os signal", .{});
-    while (should_wait()) {}
+    while (shouldWait()) {}
     log.info("exiting os signal waiting loop", .{});
 }
 
-fn http_server() void {
+fn httpServer() void {
     log.info("starting http server", .{});
-    while (should_wait()) {}
+    while (shouldWait()) {}
     log.info("http server stopped", .{});
 }
 
-fn tcp_server() void {
+fn tcpServer() void {
     log.info("starting tcp server", .{});
-    while (should_wait()) {}
+    while (shouldWait()) {}
     log.info("tcp server stopped", .{});
 }
 
-fn mining_loop(rnd: *std.rand.Xoshiro256, state: *State) !void {
+fn miningLoop(rnd: *std.rand.Xoshiro256, state: *State) !void {
     log.info("starting mining loop", .{});
     var nonce: [nonce_length]u8 = [_]u8{0} ** nonce_length;
-    while (should_wait()) {
+    while (shouldWait()) {
         // Indicate that we found new hash with required complexity.
-        var new_hash_found = false;
-        var new_block: Block = undefined;
-        while (should_wait()) {
+        var hash_found = false;
+        var block: Block = undefined;
+        while (shouldWait()) {
             std.time.sleep(10_000_000); // 10ms
-            fill_buf_random(rnd, &nonce);
+            fillBufRandom(rnd, &nonce);
             const prev_block = state.blocks.getLast();
-            new_block = Block{
+            block = Block{
                 .index = prev_block.index + 1,
                 .hash = [_]u8{0} ** hash_length,
                 .prev_hash = prev_block.hash,
@@ -205,38 +206,38 @@ fn mining_loop(rnd: *std.rand.Xoshiro256, state: *State) !void {
                 .nonce = nonce,
             };
             // To set new_block.hash.
-            const new_hash = new_block.to_hash();
-            var hash_zeros_count: complexity_type = 0;
-            for (new_hash, 0..) |byte, i| {
+            const hash = block.toHash();
+            var hash_leading_zeros: complexity = 0;
+            for (hash, 0..) |byte, i| {
                 if (byte == 0) {
-                    hash_zeros_count = @as(complexity_type, @intCast(i + 1));
+                    hash_leading_zeros = @as(complexity, @intCast(i + 1));
                     continue;
                 }
                 break;
             }
-            if (hash_zeros_count == new_block.complexity) {
-                new_hash_found = true;
+            if (hash_leading_zeros == block.complexity) {
+                hash_found = true;
                 break;
             }
-            try new_block.verify(prev_block);
+            try block.verify(prev_block);
         }
         // We need additional check there,
         // because if SIGTERM received we exit loop with new block mining.
-        if (new_hash_found) {
-            log.debug("new block is found\n{any}", .{new_block});
-            try state.blocks.append(new_block);
+        if (hash_found) {
+            log.debug("new block is found\n{any}", .{block});
+            try state.blocks.append(block);
         }
     }
     log.info("mining loop stopped", .{});
 }
 
-fn broadcast_loop() void {
+fn broadcastLoop() void {
     log.info("starting broadcast loop", .{});
-    while (should_wait()) {}
+    while (shouldWait()) {}
     log.info("broadcast loop stopped", .{});
 }
 
-fn should_wait() bool {
+fn shouldWait() bool {
     const wait_signal_state = wait_signal.load(std.builtin.AtomicOrder.monotonic);
     if (wait_signal_state) {
         // To not overload cpu.
@@ -245,7 +246,7 @@ fn should_wait() bool {
     return wait_signal_state;
 }
 
-fn fill_buf_random(rnd: *std.rand.Xoshiro256, nonce: *[nonce_length]u8) void {
+fn fillBufRandom(rnd: *std.rand.Xoshiro256, nonce: *[nonce_length]u8) void {
     for (0..nonce_length) |i| {
         nonce[i] = rnd.random().int(u8);
     }
@@ -277,7 +278,7 @@ test "test_block_encoding" {
         .complexity = 243,
         .nonce = [_]u8{3} ** nonce_length,
     };
-    const block_bytes = block.to_bytes();
+    const block_bytes = block.toBytes();
     try std.testing.expect(std.mem.eql(
         u8,
         block_bytes[0..16],
@@ -296,15 +297,15 @@ test "test_block_hash" {
         .complexity = 243,
         .nonce = [_]u8{3} ** nonce_length,
     };
-    try std.testing.expect(!block.is_hash_empty());
+    try std.testing.expect(!block.isHashEmpty());
     block.hash = [_]u8{0} ** hash_length;
     // Execute it to have block.hash is not default but calculated.
-    const hash_1 = block.to_hash();
+    const hash_1 = block.toHash();
     try std.testing.expect(!std.mem.eql(u8, &[_]u8{1} ** hash_length, &block.hash));
     // Reset hash to not use calculated hash in a new calculation.
     block.hash = [_]u8{0} ** hash_length;
-    try std.testing.expect(block.is_hash_empty());
+    try std.testing.expect(block.isHashEmpty());
     // Hashes should be equal every time.
-    const hash_2 = block.to_hash();
+    const hash_2 = block.toHash();
     try std.testing.expect(std.mem.eql(u8, &hash_1, &hash_2));
 }
