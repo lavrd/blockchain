fee4933aab4fadbb399bb770575170fc8d51bbce
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-07-18 10:54:39 +0400

    FIx building on macOS

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 uname_output=$(uname -m)
-if [[ $uname_output == "aarch64" ]]; then
+if [[ $uname_output == "arm64" ]]; then
     arch="aarch64-linux"
 elif [[ $uname_output == "x86_64" ]]; then
     arch="x86_64-linux"
 else
-    echo "Error: unknown architecture to build binary file."
+    echo "Error: unknown architecture to build binary file:" ${uname_output}
     exit 1
 fi
 
